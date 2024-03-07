85928c5799752c9d8b151b1ceea49ed53bc2b131
Maurice Mueller <maurice.mueller@bitraven.de>
2024-03-07 10:14:44 +0100

    added last updated test for new entry

--- src/test/java/de/dhbw/wikientry/WikiEntryRepositoryTest.java
+++ src/test/java/de/dhbw/wikientry/WikiEntryRepositoryTest.java
@@ -7,6 +7,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 class WikiEntryRepositoryTest {
 
     @Test
+    void testAddNewEntry() {
+        // arrange
+        WikiEntryRepository wikiEntryRepository = new WikiEntryRepository();
+        WikiEntry wikiEntry = new WikiEntry();
+
+        // act
+        wikiEntryRepository.saveWikiEntry(wikiEntry);
+
+        // assert
+        assertThat(wikiEntry.lastUpdated()).isNull();
+    }
+
+    @Test
     void testUpdateLastUpdated() {
         // arrange
         WikiEntry wikiEntry = new WikiEntry();
