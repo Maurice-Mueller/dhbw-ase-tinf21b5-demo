85fd97cb3544b9f79718d41eb09cc1658c1982db
Maurice Mueller <maurice.mueller@bitraven.de>
2024-03-07 10:44:18 +0100

    added basic entry creation

--- src/main/java/de/dhbw/wikientry/Main.java
+++ src/main/java/de/dhbw/wikientry/Main.java
@@ -8,20 +8,43 @@ public class Main {
         System.out.println("Hello in our application");
         System.out.println();
 
-        String input = null;
-        while (input == null || !input.equals("0")) {
+        WikiEntryRepository wikiEntryRepository = new WikiEntryRepository();
+
+        String menuInput = null;
+        while (menuInput == null || !menuInput.equals("0")) {
             System.out.println("What do you want to do?");
             System.out.println("[1]: Get wiki entry");
             System.out.println("[2]: New wiki entry");
             System.out.println("[0]: exit");
 
-            input = new Scanner(System.in).nextLine();
+            menuInput = new Scanner(System.in).nextLine();
 
-            if (input.equals("1")) {
-                System.out.println("GET");
-            } else if (input.equals("2")) {
-                System.out.println("NEW");
-            } else if (input.equals("0")) {
+            if (menuInput.equals("1")) {
+                System.out.println();
+                System.out.println("What entry do you want?");
+                String entryIdString = new Scanner(System.in).nextLine();
+                try {
+                    long entryId = Long.parseLong(entryIdString);
+                    WikiEntry wikiEntry = wikiEntryRepository.getWikiEntry(entryId);
+                    if(wikiEntry == null) {
+                        System.out.println("Entry does not exist.");
+                        continue;
+                    }
+                    System.out.println("Content:");
+                    System.out.println(wikiEntry.content());
+                } catch (NumberFormatException exception) {
+                    System.out.println("Wrong input");
+                }
+
+            } else if (menuInput.equals("2")) {
+                System.out.println();
+                System.out.println("Enter content:");
+                String content = new Scanner(System.in).nextLine();
+                System.out.println("Enter author:");
+                String author = new Scanner(System.in).nextLine();
+                WikiEntry entry = new WikiEntry(content, author);
+                wikiEntryRepository.saveWikiEntry(entry);
+            } else if (menuInput.equals("0")) {
                 System.out.println("Good bye.");
             } else {
                 System.out.println("Wrong input");
