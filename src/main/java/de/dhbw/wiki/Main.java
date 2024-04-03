cdfb02a3046954e52274e1f285a7ee8267c96d94
Maurice Müller <maurice.mueller@bitraven.de>
2024-04-03 23:50:45 +0200

    compiler warnings as errors

--- src/main/java/de/dhbw/wiki/Main.java
+++ src/main/java/de/dhbw/wiki/Main.java
@@ -2,9 +2,12 @@ package de.dhbw.wiki;
 
 import de.dhbw.wiki.view.MainView;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
+        var date = new ArrayList(); // example for compiler warning
         WikiEntryRepository wikiEntryRepository = new WikiEntryRepository();
         WikiEntryService wikiEntryService = new WikiEntryService(wikiEntryRepository);
         new MainView(wikiEntryService).show();
